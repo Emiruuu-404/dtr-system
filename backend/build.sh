5f81b15ca4e12422053b8b3a5a7d4880e1e46eec
Emiruuu <emiruu@cbsua.edu.ph>
2026-03-18 15:55:02 +0800

    build.sh fix

--- backend/build.sh
+++ backend/build.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
+pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# Load existing data from local database (runs only if datadump.json exists)
-if [ -f "datadump.json" ]; then
-    python manage.py loaddata datadump.json
-    echo "Data loaded successfully!"
-fi
